f300cfdce1e98beb98d2554aba99fef9fbbc6ed8
Ivan Mahonin <bh@icystar.com>
2016-09-26 21:44:43 +0700

    fix restore_owner.sh

--- restore_owner.sh
+++ restore_owner.sh
@@ -4,4 +4,10 @@ SCRIPT_FILE=`realpath "$0"`
 BASE_DIR=`dirname "$SCRIPT_FILE"`
 DATA_DIR="$BASE_DIR/docker-builder-data"
 
-sudo chown -R `id -un`:`id -gn` $DATA_DIR
+CONFIG_FILE="$BASE_DIR/config.sh"
+PACKET_BUILD_DIR="$DATA_DIR/build/packet"
+if [ -f $CONFIG_FILE ]; then
+	source $CONFIG_FILE
+fi
+
+sudo chown -R `id -un`:`id -gn` $PACKET_BUILD_DIR
