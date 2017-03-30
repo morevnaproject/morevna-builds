699c617494c2406ab5587c9905c5d12c3c19cb91
Ivan Mahonin <bh@icystar.com>
2017-03-30 18:10:18 +0700

    Fix build for PapagayoNG

---
+++ build-papagayong.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+BASE_DIR=$(cd `dirname "$0"`; pwd)
+DATA_DIR="$BASE_DIR/docker-builder-data"
+BUILD_DIR=$DATA_DIR/build
+PUBLISH_DIR=$BASE_DIR/publish
+CONFIG_FILE="$BASE_DIR/config.sh"
+PACKET_BUILD_DIR="$BUILD_DIR/packet"
+SCRIPT_BUILD_DIR="$BUILD_DIR/script"
+if [ -f $CONFIG_FILE ]; then
+	source $CONFIG_FILE
+fi
+
+SCRIPT="$BASE_DIR/docker/run.sh"
+
+run_appimage() {
+    export PLATFORM="$1"
+    export ARCH="$2"
+
+    echo ""
+    echo "Update papagayong for $PLATFORM-$ARCH"
+    echo ""
+    $SCRIPT update papagayong-master
+    $SCRIPT clean_before_do install_release papagayong-appimage
+
+    "$PUBLISH_DIR/publish.sh" \
+        "papagayong" \
+        "PapagayoNG-%VERSION%-%DATE%-%COMMIT%-$PLATFORM-${ARCH}bits.appimage" \
+        "$PACKET_BUILD_DIR/$PLATFORM-$ARCH/papagayong-appimage/install_release" \
+        "*.appimage" \
+        "$PACKET_BUILD_DIR/$PLATFORM-$ARCH/papagayong-appimage/envdeps_release/version-papagayong-master"
+}
+
+run_nsis() {
+    export PLATFORM="$1"
+    export ARCH="$2"
+
+    echo ""
+    echo "Update papagayong for $PLATFORM-$ARCH"
+    echo ""
+    #$SCRIPT update papagayong-master
+    #$SCRIPT clean_before_do unpack papagayong-master
+
+    # QUICK HACK:
+    $SCRIPT shell papagayong-master "/build/script/packet/papagayong-master.files/build-win.sh"
+
+    "$PUBLISH_DIR/publish.sh" \
+        "papagayong" \
+        "PapagayoNG-%VERSION%-%DATE%-%COMMIT%-$PLATFORM-${ARCH}bits.exe" \
+        "$PACKET_BUILD_DIR/$PLATFORM-$ARCH/papagayong-master/build" \
+        "*.exe" \
+        "$PACKET_BUILD_DIR/$PLATFORM-$ARCH/papagayong-master/unpack/version-papagayong-master"
+}
+
+#run_appimage linux 64
+#run_appimage linux 32
+run_nsis win 32
--- docker-builder-data/build/script/packet/papagayong-master.files/build-win.sh
+++ docker-builder-data/build/script/packet/papagayong-master.files/build-win.sh
@@ -4,39 +4,37 @@
 
 set -e
 
-ROOT_DIR=$1
-FILES_DIR=$(cd `dirname "$0"`; pwd)
-SOURCE_DIR="$ROOT_DIR/unpack/papagayo-ng"
-BUILD_DIR="$ROOT_DIR/build"
+PK_DIRNAME="papagayo-ng"
+
 PREBUILT_URL="https://github.com/morevnaproject/papagayo-ng/releases/download/v1.4.0/papagayo-ng-1.4.0-win.zip"
 PREBUILT_ZIP="papagayo-ng-1.4.0-win.zip"
 PREBUILT_DIR="papagayo-ng-1.4.0-win"
 
-VERSION=$(grep "export VERSION=" "$SOURCE_DIR/util/package-linux.sh" | cut -d\' -f 2)
+VERSION=$(grep "export VERSION=" "$UNPACK_PACKET_DIR/$PK_DIRNAME/util/package-linux.sh" | cut -d\' -f 2)
 TARGET_DIR="papagayo-ng-$VERSION-win"
 
-if [ ! -f "$BUILD_DIR/papagayo-ng-$VERSION-win-installer.exe" ] \
-|| [ "$BUILD_DIR/papagayo-ng-$VERSION-win-installer.exe" -ot "$ROOT_DIR/unpack.done" ]; then
-	mkdir -p "$BUILD_DIR/prebuilt"
+if [ ! -f "$BUILD_PACKET_DIR/papagayo-ng-$VERSION-win-installer.exe" ] \
+|| [ "$BUILD_PACKET_DIR/papagayo-ng-$VERSION-win-installer.exe" -ot "$CURRENT_PACKET_DIR/unpack.done" ]; then
+	mkdir -p "$BUILD_PACKET_DIR/prebuilt"
 	
-	cd "$BUILD_DIR/prebuilt"
+	cd "$BUILD_PACKET_DIR/prebuilt"
 	wget -c "$PREBUILT_URL"
 	rm -rf "$TARGET_DIR"
 	unzip "$PREBUILT_ZIP"
 	
-	cd "$BUILD_DIR"
+	cd "$BUILD_PACKET_DIR"
 	rm -f "$TARGET_DIR.zip"
 	rm -rf "$TARGET_DIR"
 	mv "prebuilt/$PREBUILT_DIR" "$TARGET_DIR"
 	
-	cd "$BUILD_DIR/$TARGET_DIR"
+	cd "$BUILD_PACKET_DIR/$TARGET_DIR"
 	rm -rf papagayo-ng
-	ln -s "$SOURCE_DIR" papagayo-ng
-	cp "$FILES_DIR/papagayo-ng.nsi" .
-	cp "$FILES_DIR/papagayo-ng.bat" .
-		makensis papagayo-ng.nsi
+	ln -s "$UNPACK_PACKET_DIR/$PK_DIRNAME" papagayo-ng
+	cp "$FILES_PACKET_DIR/papagayo-ng.nsi" .
+	cp "$FILES_PACKET_DIR/papagayo-ng.bat" .
+	makensis papagayo-ng.nsi
 	
-	cd "$BUILD_DIR"
+	cd "$BUILD_PACKET_DIR"
 	zip -r "$TARGET_DIR.zip" "$TARGET_DIR"
 	rm -rf "$TARGET_DIR"
 	mv "papagayo-ng-installer.exe" "papagayo-ng-$VERSION-win-installer.exe"
