c7de4b55fb25ad0e479056036c766e52af3168f5
Ivan Mahonin <bh@icystar.com>
2017-03-31 14:28:00 +0700

    Switch libmypaint repo

--- docker-builder-data/build/script/packet/mypaintlib-master.sh
+++ docker-builder-data/build/script/packet/mypaintlib-master.sh
@@ -2,7 +2,7 @@ DEPS="jsonc-0.12.1 glib-2.50.0"
 DEPS_NATIVE="automake-1.15"
 
 PK_DIRNAME="libmypaint"
-PK_URL="https://github.com/mypaint/$PK_DIRNAME.git"
+PK_URL="https://github.com/blackwarthog/$PK_DIRNAME.git"
 
 source $INCLUDE_SCRIPT_DIR/inc-pkall-git.sh
 
