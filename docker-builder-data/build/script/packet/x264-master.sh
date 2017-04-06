13788712b578abd7021d1fc64a9cda1c4f550de9
Ivan Mahonin <bh@icystar.com>
2017-04-06 12:14:17 +0700

    Fix x264: set AS environment variable to 'yasm'

--- docker-builder-data/build/script/packet/x264-master.sh
+++ docker-builder-data/build/script/packet/x264-master.sh
@@ -4,3 +4,19 @@ PK_DIRNAME="x264"
 PK_URL="http://git.videolan.org/git/$PK_DIRNAME.git"
 
 source $INCLUDE_SCRIPT_DIR/inc-pkall-git.sh
+
+pkbuild() {
+    cd "$BUILD_PACKET_DIR/$PK_DIRNAME" || return 1
+    
+    if ! check_packet_function $NAME build.cunfigure; then
+        AS=yasm ./configure \
+         $PK_CONFIGURE_OPTIONS_DEFAULT \
+         $PK_CONFIGURE_OPTIONS \
+         || return 1
+        set_done $NAME build.cunfigure
+    fi
+    
+    if ! AS=yasm make -j${THREADS}; then
+        return 1
+    fi
+}
