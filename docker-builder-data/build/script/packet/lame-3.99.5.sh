336ab7fa29fefa60137fe01957c8f0cff0fad138
Ivan Mahonin <bh@icystar.com>
2017-04-08 13:48:45 +0700

    fix lame for win-32

--- docker-builder-data/build/script/packet/lame-3.99.5.sh
+++ docker-builder-data/build/script/packet/lame-3.99.5.sh
@@ -5,3 +5,7 @@ PK_ARCHIVE="$PK_DIRNAME.tar.gz"
 PK_URL="https://sourceforge.net/projects/lame/files/lame/3.99/$PK_ARCHIVE/download"
 
 source $INCLUDE_SCRIPT_DIR/inc-pkall-default.sh
+
+if [ "$PLATFORM" = "win" ] && [ "$ARCH" = "32" ]; then
+    PK_CFLAGS="-msse"
+fi
