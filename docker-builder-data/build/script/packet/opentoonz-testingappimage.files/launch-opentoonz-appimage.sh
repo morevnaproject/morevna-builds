382f872a98d1d21979ca375ba26f96adc7071736
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2017-07-21 19:06:39 +0700

    Rebranding for OpenToonz Morevna Edition

---
+++ docker-builder-data/build/script/packet/opentoonz-testingappimage.files/launch-opentoonz-appimage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+OLDDIR=`pwd`
+cd `dirname "$0"`
+SCRIPT_DIR=`pwd`
+BASE_DIR=`dirname "$SCRIPT_DIR"`
+
+export LD_LIBRARY_PATH="$BASE_DIR/lib:$BASE_DIR/lib/opentoonz:$BASE_DIR/lib64:$LD_LIBRARY_PATH"
+export QT_XKB_CONFIG_ROOT=$QT_XKB_CONFIG_ROOT:/usr/local/share/X11/xkb:/usr/share/X11/xkb
+
+if [ "$1" = "--appimage-exec" ]; then
+	if ! "${@:2}"; then
+		cd "$OLDDIR"
+		exit 1
+	fi
+else
+	if ! "./launch-opentoonz.sh.wrapper" "$@"; then
+		cd "$OLDDIR"
+		exit 1
+	fi
+fi
+cd "$OLDDIR"
