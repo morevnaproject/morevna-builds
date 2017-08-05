fda0a7b0320923e65c21c85b4103ee8529818afb
Ivan Mahonin <bh@icystar.com>
2017-08-05 19:29:37 +0700

    add libmypaint dependency into opentoonz-master

--- docker-builder-data/build/script/packet/opentoonz-testing.sh
+++ docker-builder-data/build/script/packet/opentoonz-testing.sh
@@ -1,8 +1,5 @@
 source "$PACKET_SCRIPT_DIR/opentoonz-master.sh"
 
-DEPS="$DEPS mypaintlib-master"
 PK_DIRNAME="opentoonz"
 PK_URL="https://github.com/blackwarthog/$PK_DIRNAME.git"
 PK_GIT_CHECKOUT="origin/testing"
-
-PK_LICENSE_FILES="$PK_LICENSE_FILES stuff/library/mypaint?brushes/Licenses.txt"
