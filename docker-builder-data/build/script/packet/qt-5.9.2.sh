64ee37ec96c48e78397e43875a638f45fb279a1e
Ivan Mahonin <bh@icystar.com>
2017-11-20 04:00:46 +0000

    qt-5.9.2 mingw

--- docker-builder-data/build/script/packet/qt-5.9.2.sh
+++ docker-builder-data/build/script/packet/qt-5.9.2.sh
@@ -14,21 +14,38 @@ fi
 
 pkbuild() {
     cd "$BUILD_PACKET_DIR/$PK_DIRNAME"
-    
+
     if ! check_packet_function $NAME build.configure; then
         local LOCAL_OPTIONS=
         if [ "$PLATFORM" = "win" ]; then
-            #rm -f "qtbase/mkspecs/win32-g++/qmake.conf"
-            #patch "$UNPACK_PACKET_DIR/$PK_DIRNAME/qtbase/mkspecs/win32-g++/qmake.conf" \
-            # -i "$FILES_PACKET_DIR/qmake.conf.patch" -o - \
-            # > "qtbase/mkspecs/win32-g++/qmake.conf"
+            rm -f "qtbase/mkspecs/win32-g++/qmake.conf"
+            patch "$UNPACK_PACKET_DIR/$PK_DIRNAME/qtbase/mkspecs/win32-g++/qmake.conf" \
+             -i "$FILES_PACKET_DIR/qmake.conf.patch" -o - \
+             > "qtbase/mkspecs/win32-g++/qmake.conf"
 
-            #rm -f "qtactiveqt/src/tools/idc/idc.pro"
-            #patch "$UNPACK_PACKET_DIR/$PK_DIRNAME/qtactiveqt/src/tools/idc/idc.pro" \
-            # -i "$FILES_PACKET_DIR/idc.pro.patch" -o - \
-            # > "qtactiveqt/src/tools/idc/idc.pro"
+            rm -f "qtactiveqt/src/tools/idc/idc.pro"
+            patch "$UNPACK_PACKET_DIR/$PK_DIRNAME/qtactiveqt/src/tools/idc/idc.pro" \
+             -i "$FILES_PACKET_DIR/idc.pro.patch" -o - \
+             > "qtactiveqt/src/tools/idc/idc.pro"
 
-            LOCAL_OPTIONS="-xplatform win32-g++ -device-option CROSS_COMPILE=$HOST-"
+            rm -f "qtbase/src/plugins/platforms/direct2d/direct2d.pro"
+            patch "$UNPACK_PACKET_DIR/$PK_DIRNAME/qtbase/src/plugins/platforms/direct2d/direct2d.pro" \
+             -i "$FILES_PACKET_DIR/direct2d.pro.patch" -o - \
+             > "qtbase/src/plugins/platforms/direct2d/direct2d.pro"
+
+            # mingw bugs workarounds (https://sourceforge.net/p/mingw-w64/mailman/message/35627786/):
+
+            rm -f "qtbase/src/plugins/platforms/direct2d/qwindowsdirect2dpaintengine.cpp"
+            patch "$UNPACK_PACKET_DIR/$PK_DIRNAME/qtbase/src/plugins/platforms/direct2d/qwindowsdirect2dpaintengine.cpp" \
+             -i "$FILES_PACKET_DIR/qwindowsdirect2dpaintengine.cpp.patch" -o - \
+             > "qtbase/src/plugins/platforms/direct2d/qwindowsdirect2dpaintengine.cpp"
+
+            rm -f "qtbase/src/plugins/platforms/direct2d/qwindowsdirect2dintegration.cpp"
+            patch "$UNPACK_PACKET_DIR/$PK_DIRNAME/qtbase/src/plugins/platforms/direct2d/qwindowsdirect2dintegration.cpp" \
+             -i "$FILES_PACKET_DIR/qwindowsdirect2dintegration.cpp.patch" -o - \
+             > "qtbase/src/plugins/platforms/direct2d/qwindowsdirect2dintegration.cpp"
+
+            LOCAL_OPTIONS="-xplatform win32-g++ -device-option CROSS_COMPILE=$HOST- -opengl desktop"
         fi
 
         rm -f "qtserialbus/src/plugins/canbus/socketcan/socketcanbackend.cpp"
@@ -41,13 +58,14 @@ pkbuild() {
            -prefix "$INSTALL_PACKET_DIR" \
            $LOCAL_OPTIONS \
            -release \
+           -shared \
            -opensource -confirm-license \
            -nomake examples \
          || return 1
 
         set_done $NAME build.configure
     fi
-    
+
     native_at_place make -j${THREADS} || native_at_place make ||return 1
 }
 
