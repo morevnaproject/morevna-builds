f00a94bc276864f5f9ffba3c0b813583ef52def2
Ivan Mahonin <bh@icystar.com>
2017-12-19 22:26:01 +0700

    Take opentoonz-testing from morevnaproject

--- docker-builder-data/build/script/packet/opentoonz-testing.sh
+++ docker-builder-data/build/script/packet/opentoonz-testing.sh
@@ -1,5 +1,5 @@
 source "$PACKET_SCRIPT_DIR/opentoonz-master.sh"
 
 PK_DIRNAME="opentoonz"
-PK_URL="https://github.com/blackwarthog/$PK_DIRNAME.git"
+PK_URL="https://github.com/morevnaproject/$PK_DIRNAME.git"
 PK_GIT_CHECKOUT="origin/testing"
