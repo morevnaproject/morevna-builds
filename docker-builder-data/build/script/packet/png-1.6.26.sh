fec6b54038d174ba7756fa59683b1ba64781bd51
Ivan Mahonin <bh@icystar.com>
2017-12-22 05:44:18 +0000

    fix libpng

--- docker-builder-data/build/script/packet/png-1.6.26.sh
+++ docker-builder-data/build/script/packet/png-1.6.26.sh
@@ -5,3 +5,7 @@ PK_ARCHIVE="$PK_DIRNAME.tar.gz"
 PK_URL="http://download.sourceforge.net/libpng/$PK_ARCHIVE"
 
 source $INCLUDE_SCRIPT_DIR/inc-pkall-default.sh
+
+pkhook_prebuild() {
+    pkhelper_patch . libpng.pc.in
+}
