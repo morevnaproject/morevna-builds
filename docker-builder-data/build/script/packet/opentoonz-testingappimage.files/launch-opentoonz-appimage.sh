78aee8977caf657af315de7b51a63df00e262839
Ivan Mahonin <bh@icystar.com>
2018-01-24 20:45:28 +0000

    fix pulseaudio for opentoonz appimage

--- docker-builder-data/build/script/packet/opentoonz-testingappimage.files/launch-opentoonz-appimage.sh
+++ docker-builder-data/build/script/packet/opentoonz-testingappimage.files/launch-opentoonz-appimage.sh
@@ -5,7 +5,7 @@ cd `dirname "$0"`
 SCRIPT_DIR=`pwd`
 BASE_DIR=`dirname "$SCRIPT_DIR"`
 
-export LD_LIBRARY_PATH="$BASE_DIR/lib:$BASE_DIR/lib/opentoonz:$BASE_DIR/lib64:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$BASE_DIR/lib:$BASE_DIR/lib/opentoonz:$BASE_DIR/lib/pulseaudio:$BASE_DIR/lib64:$LD_LIBRARY_PATH"
 export QT_XKB_CONFIG_ROOT=$QT_XKB_CONFIG_ROOT:/usr/local/share/X11/xkb:/usr/share/X11/xkb
 
 if [ "$1" = "--appimage-exec" ]; then
