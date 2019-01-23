606960ff36f7ad0ebd6dc1c7982f8786b646d1a8
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2019-01-23 23:19:23 +0700

    Disable zlib quickhack

--- build-synfigstudio.sh
+++ build-synfigstudio.sh
@@ -49,7 +49,7 @@ run_nsis() {
     echo ""
     echo "Update and build synfigstudio for $PLATFORM-$ARCH"
     echo ""
-    PLATFORM=win ARCH=32 $SCRIPT clean_before_do env zlib-1.2.11 # for NSIS
+    #PLATFORM=win ARCH=32 $SCRIPT clean_before_do env zlib-1.2.11 # for NSIS
     $SCRIPT chain native update synfigetl-master \
             chain native update synfigcore-master \
             chain update synfigetl-master \
