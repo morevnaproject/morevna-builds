a9c4ec0b215a03ffeb76329a53a7aef7b0b9e6c6
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2020-06-11 14:13:55 +0700

    Fix url

--- env-builder-data/build/script/packet/opentoonz-testing.sh
+++ env-builder-data/build/script/packet/opentoonz-testing.sh
@@ -1,5 +1,5 @@
 source "$PACKET_SCRIPT_DIR/opentoonz-master.sh"
 
 PK_DIRNAME="opentoonz"
-PK_URL="https://github.com/morevnaproject/$PK_DIRNAME.git"
+PK_URL="https://github.com/morevnaproject-org/$PK_DIRNAME.git"
 PK_GIT_CHECKOUT="origin/testing"
