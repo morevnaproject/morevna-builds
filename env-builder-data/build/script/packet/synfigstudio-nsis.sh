morevnaproject/morevna-builds

commit 89acaacad91cb0fb0d9fc3c3e5787397d51eb359

Add gspawn-win*-helper* binaries to Synfig Windows build. Closes #20.

env-builder-data/build/script/packet/synfigstudio-nsis.sh
@@ -72,6 +72,7 @@ pkinstall_release() {
             gdk-pixbuf-pixdata.exe \
             gdk-pixbuf-query-loaders.exe \
             gio-querymodules.exe \
+            gspawn-win*-helper-* \
             melt.exe \
             sox.exe \
             synfig.exe \

env-builder-data/build/script/packet/synfigstudio-portable.sh
@@ -38,6 +38,7 @@ pkinstall_release() {
             gdk-pixbuf-pixdata.exe \
             gdk-pixbuf-query-loaders.exe \
             gio-querymodules.exe \
+            gspawn-win*-helper-* \
             melt.exe \
             sox.exe \
             synfig.exe \

commit 6af2ded4bb47f4d1a373a1de98d1cdba54106bfa

Fix build error introduced in 89acaacad91cb0fb0d9f

env-builder-data/build/script/packet/synfigstudio-nsis.sh
@@ -77,7 +77,7 @@ pkinstall_release() {
             sox.exe \
             synfig.exe \
             synfigstudio.exe; do
-        cp -rf "$ENVDEPS_RELEASE_PACKET_DIR/bin/${FILE}" "./bin/" || return 1
+        cp -rf "$ENVDEPS_RELEASE_PACKET_DIR/bin/"${FILE} "./bin/" || return 1
     done
     copy "$ENVDEPS_RELEASE_PACKET_DIR/etc/" "./etc/" || return 1
     [ -d "./lib/gdk-pixbuf-2.0/2.10.0/loaders" ] || mkdir -p "./lib/gdk-pixbuf-2.0/2.10.0/loaders"

env-builder-data/build/script/packet/synfigstudio-portable.sh
@@ -43,7 +43,7 @@ pkinstall_release() {
             sox.exe \
             synfig.exe \
             synfigstudio.exe; do
-        cp -rf "$ENVDEPS_RELEASE_PACKET_DIR/bin/${FILE}" "./bin/" || return 1
+        cp -rf "$ENVDEPS_RELEASE_PACKET_DIR/bin/"${FILE} "./bin/" || return 1
     done
     copy "$ENVDEPS_RELEASE_PACKET_DIR/etc/" "./etc/" || return 1
     [ -d "./lib/gdk-pixbuf-2.0/2.10.0/loaders" ] || mkdir -p "./lib/gdk-pixbuf-2.0/2.10.0/loaders"