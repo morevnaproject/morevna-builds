85c15a78fa5baac253347c4b3bd09bbbeab17c6e
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2021-10-20 04:10:05 +0700

    Add fribidi package (required for Synfig 1.5.1)

---
+++ env-builder-data/build/script/packet/fribidi-0.19.7.sh
@@ -0,0 +1,5 @@
+PK_DIRNAME="fribidi-0.19.7"
+PK_ARCHIVE="$PK_DIRNAME.tar.bz2"
+PK_URL="https://github.com/fribidi/fribidi/releases/download/0.19.7/$PK_ARCHIVE"
+
+source $INCLUDE_SCRIPT_DIR/inc-pkall-default.sh
