36694a8d53d482fd82bd868416351a55f447d3ab
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2022-04-13 23:05:42 +0700

    Optimize build of gtk and synfigstudio on Linux

--- env-builder-data/build/script/packet/gtk-3.22.12.sh
+++ env-builder-data/build/script/packet/gtk-3.22.12.sh
@@ -1,15 +1,16 @@
 DEPS="epoxy-master rsvg-2.40.16 atk-2.22.0 gdkpixbuf-2.36.0 cairo-1.15.4 pango-1.40.3"
-DEPS_NATIVE="gtk-3.22.12"
+if [ "$PLATFORM" != "linux" ]; then
+    DEPS_NATIVE="gtk-3.22.12"
+fi
+if [ "$PLATFORM" = "linux" ] || [ ! -z "$IS_NATIVE" ]; then
+    DEPS="$DEPS atspi2atk-2.22.0"
+fi
 
 PK_DIRNAME="gtk+-3.22.12"
 PK_ARCHIVE="$PK_DIRNAME.tar.xz"
 PK_URL="https://download.gnome.org/sources/gtk+/3.22/$PK_ARCHIVE"
 
 source $INCLUDE_SCRIPT_DIR/inc-pkall-default.sh
-
-if [ "$PLATFORM" = "linux" ] || [ ! -z "$IS_NATIVE" ]; then
-    DEPS="$DEPS atspi2atk-2.22.0"
-fi
 
 if [ "$PLATFORM" = "win" ]; then
     PK_CONFIGURE_OPTIONS="--enable-introspection=no"
