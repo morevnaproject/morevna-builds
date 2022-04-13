9b76b649a235a94f202dcc6455ae4396b2641328
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2022-04-13 23:03:08 +0700

    Fix glib build

--- env-builder-data/build/script/packet/glib-2.69.3.sh
+++ env-builder-data/build/script/packet/glib-2.69.3.sh
@@ -1,5 +1,7 @@
-DEPS="zlib-1.2.11 ffi-3.2.1"
-#DEPS_NATIVE="glib-2.69.3"
+DEPS="zlib-1.2.11 ffi-3.2.1 pcre-8.37"
+if [ "$PLATFORM" != "linux" ]; then
+    DEPS_NATIVE="glib-2.69.3"
+fi
 
 PK_DIRNAME="glib-2.69.3"
 PK_ARCHIVE="$PK_DIRNAME.tar.xz"
@@ -12,7 +14,7 @@ source $INCLUDE_SCRIPT_DIR/inc-pkall-default.sh
 
 pkbuild() {
     cd "$BUILD_PACKET_DIR/$PK_DIRNAME"
-    sed -i 's|https://ftp.pcre.org|http://ftp.exim.org|g' subprojects/libpcre.wrap
+    #sed -i 's|https://ftp.pcre.org|http://ftp.exim.org|g' subprojects/libpcre.wrap
     if [[ $TC_HOST == "i686-w64-mingw32" ]]; then
         export WINEPATH="$ENVDEPS_PACKET_DIR/bin/"
         export CROSS_FILE="--cross-file /build/script/meson/linux-mingw-w64-32bit.txt"
@@ -22,11 +24,25 @@ pkbuild() {
     else
         export CROSS_FILE=""
     fi
-    meson _build $CROSS_FILE -Dprefix=${INSTALL_PACKET_DIR} || return 1
+    if [ -d _build ]; then
+        export RECONFIGURE="--reconfigure"
+    fi
+    meson $RECONFIGURE _build $CROSS_FILE -Dprefix=${INSTALL_PACKET_DIR} || return 1
     ninja -C _build || return 1
+    
 }
 
 pkinstall() {
     cd "$BUILD_PACKET_DIR/$PK_DIRNAME"
     ninja -C _build install || return 1
+    
+    if [ "$PLATFORM" = "win" ]; then
+#        cat <<EOT >> ${INSTALL_PACKET_DIR}/bin/glib-compile-resources
+##!/bin/bash
+#
+#glib-compile-resources "$@"
+#EOT
+#        chmod +x ${INSTALL_PACKET_DIR}/bin/glib-compile-resources
+        sed -i 's|glib_compile_resources=.*|glib_compile_resources=glib-compile-resources|g' ${INSTALL_PACKET_DIR}/lib/pkgconfig/gio-2.0.pc
+    fi
 }
---
+++ env-builder-data/build/script/packet/pcre-8.37.sh
@@ -0,0 +1,6 @@
+PK_DIRNAME="pcre-8.37"
+PK_ARCHIVE="$PK_DIRNAME.tar.bz2"
+PK_URL="https://ftp.exim.org/pub/pcre/$PK_ARCHIVE"
+PK_LICENSE_FILES="AUTHORS COPYING"
+
+source $INCLUDE_SCRIPT_DIR/inc-pkall-default.sh
