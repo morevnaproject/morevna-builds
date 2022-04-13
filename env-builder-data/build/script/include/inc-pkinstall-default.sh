6e631035dfd275385b731289ad48c98b86abf869
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2022-04-13 23:10:23 +0700

    Avoid "directory exist" error during install stage (#49)

--- env-builder-data/build/script/include/inc-pkinstall-default.sh
+++ env-builder-data/build/script/include/inc-pkinstall-default.sh
@@ -2,6 +2,9 @@
 # PK_DIRNAME
 
 pkinstall() {
+    [ ! -d "$INSTALL_PACKET_DIR" ] || rm -rf "$INSTALL_PACKET_DIR"
+    mkdir -p "$INSTALL_PACKET_DIR"
+    
     cd "$BUILD_PACKET_DIR/$PK_DIRNAME"
     if ! make install; then
         return 1
