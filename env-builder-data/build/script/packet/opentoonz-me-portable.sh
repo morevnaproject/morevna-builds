b0e631413faeb19966af6d63cc9cb97c9707e66c
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2022-07-19 23:44:57 +0700

    Fix build script for OpenToonz Morevna Edition

--- env-builder-data/build/script/packet/opentoonz-me-portable.sh
+++ env-builder-data/build/script/packet/opentoonz-me-portable.sh
@@ -1,3 +1,3 @@
 source "$PACKET_SCRIPT_DIR/opentoonz-master-portable.sh"
 
-DEPS=`echo "$DEPS" | sed "s|opentoonz-master|opentoonz-testing|g"`
+DEPS=`echo "$DEPS" | sed "s|opentoonz-master|opentoonz-me|g"`
