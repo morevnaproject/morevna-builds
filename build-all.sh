92992075ab3c0696e2d7437bda456d07e10e1ff6
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2022-07-24 21:05:16 +0700

    Build Synfig before OpenToonz

--- build-all.sh
+++ build-all.sh
@@ -4,8 +4,8 @@ set -e
 
 BASE_DIR=$(cd `dirname "$0"`; pwd)
 
-#"$BASE_DIR/build-opentoonz.sh"
-"$BASE_DIR/build-opentoonz-me.sh"
 "$BASE_DIR/build-synfigstudio.sh"
 #"$BASE_DIR/build-synfigstudio-debug.sh"
+#"$BASE_DIR/build-opentoonz.sh"
+"$BASE_DIR/build-opentoonz-me.sh"
 #"$BASE_DIR/build-papagayong.sh"
