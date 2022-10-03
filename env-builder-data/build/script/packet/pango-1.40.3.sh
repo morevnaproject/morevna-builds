morevnaproject/morevna-builds

commit 34c2178c661fdde7046d22bbffc4c72d3c5755fb

Use newer compiler for MinGW64 cross-compilation (#79)
Now we use separate docker image for mingw cross-compilation.
    Based on Debian 11 (bullseye).

    - gcc 10.2.1
    - mingw 8.0.0

docker/mingw/Dockerfile
@@ -1,0 +1,150 @@
+FROM buildpack-deps:bullseye-curl
+
+# Install deps
+RUN set -x \
+ && apt-get update                                     \
+ && apt-get install -y -q                              \
+        autoconf                                       \
+        automake                                       \
+        autotools-dev                                  \
+        bc                                             \
+        binfmt-support                                 \
+#        binutils-multiarch                             \
+#        binutils-multiarch-dev                         \
+        build-essential                                \
+        ccache                                         \
+        clang                                          \
+        curl                                           \
+        devscripts                                     \
+        gdb                                            \
+        git-core                                       \
+        libtool                                        \
+        llvm                                           \
+#        mercurial                                      \
+#        multistrap                                     \
+        patch                                          \
+        software-properties-common                     \
+        subversion                                     \
+        wget                                           \
+        xz-utils                                       \
+        cmake                                          \
+#        qemu-user-static                               \
+#        libxml2-dev                                    \
+#        lzma-dev                                       \
+        openssl                                        \
+#        libssl-dev                                     \
+ && apt-get clean
+
+# Install Windows cross-tools
+RUN apt-get install -y mingw-w64 \
+ && apt-get clean
+
+
+# Create symlinks for triples and set default CROSS_TRIPLE
+ENV WINDOWS_TRIPLES=i686-w64-mingw32,x86_64-w64-mingw32                                                                           \
+    CROSS_TRIPLE=x86_64-linux-gnu
+RUN mkdir -p /usr/x86_64-linux-gnu;                                                               \
+    for triple in $(echo ${WINDOWS_TRIPLES} | tr "," " "); do                                     \
+      mkdir -p /usr/$triple/bin;                                                                  \
+      for bin in /etc/alternatives/$triple-* /usr/bin/$triple-*; do                               \
+        if [ ! -f /usr/$triple/bin/$(basename $bin | sed "s/$triple-//") ]; then                  \
+          ln -s $bin /usr/$triple/bin/$(basename $bin | sed "s/$triple-//");                      \
+        fi;                                                                                       \
+      done;                                                                                       \
+      ln -s gcc /usr/$triple/bin/cc;                                                              \
+      ln -s /usr/$triple /usr/x86_64-linux-gnu/$triple;                                           \
+    done
+
+RUN apt-get update && \
+DEBIAN_FRONTEND=noninteractive apt-get install -y -q \
+pkg-config \
+xsltproc \
+m4 \
+automake \
+autopoint \
+intltool \
+libtool \
+libltdl-dev \
+git \
+libintl-perl \
+python3-pip \
+ninja-build \
+doxygen \
+graphviz \
+python \
+# for synfigstudio-nsis \
+unzip \
+# for portable versions \
+zip \
+# fix ca-certificates issue - https://github.com/multiarch/crossbuild/issues/63
+#libssl1.0.2 openssl libgnutls30 \
+# epoxy
+xutils-dev \
+# native-SDL
+libx11-dev \
+libxext-dev \
+# native-boost
+libbz2-dev \
+# native-cairo
+libxcb1-dev \
+# native-gtk -> native-epoxy
+libegl1-mesa-dev \
+# native-gtk -> native-atspi2
+libxtst-dev \
+libdbus-1-dev \
+# qt deps see: http://doc.qt.io/qt-5/linux-requirements.html \
+#libxrender-dev \
+#libfontconfig1-dev \
+#libfreetype6-dev \
+#libxi-dev \
+#libx11-xcb-dev \
+#libsm-dev \
+#libice-dev \
+#libglu1-mesa-dev \
+# other deps \
+#libdirectfb-dev \
+#liblzma-dev \
+#liblzo2-dev \
+#libudev-dev \
+#libfuse-dev \
+#libdb-dev \
+#libasound2-dev \
+#libffi-dev \
+#libmount-dev \
+#libbz2-dev \
+#libcroco3-dev \
+#libpthread-stubs0-dev \
+#libxau-dev \
+#libxcursor-dev \
+#flex \
+#bison \
+#python-dev \
+#xutils-dev \
+&& apt-get clean
+
+COPY files/install-cross-tools.sh /
+RUN /install-cross-tools.sh
+#TODO: Remove above lines after Conan migration?
+
+RUN update-alternatives --set i686-w64-mingw32-gcc /usr/bin/i686-w64-mingw32-gcc-posix && \
+    update-alternatives --set i686-w64-mingw32-g++ /usr/bin/i686-w64-mingw32-g++-posix && \
+    update-alternatives --set x86_64-w64-mingw32-gcc /usr/bin/x86_64-w64-mingw32-gcc-posix && \
+    update-alternatives --set x86_64-w64-mingw32-g++ /usr/bin/x86_64-w64-mingw32-g++-posix
+
+RUN pip3 install meson
+
+RUN cd /tmp \
+  && wget http://ftp.us.debian.org/debian/pool/main/a/automake-1.15/automake-1.15_1.15.1-5_all.deb \
+  && dpkg -i automake-1.15_1.15.1-5_all.deb \
+  && rm -f automake-1.15_1.15.1-5_all.deb
+
+RUN dpkg --add-architecture i386 \
+    && wget -qO - https://dl.winehq.org/wine-builds/winehq.key | apt-key add - \
+    && apt-add-repository https://dl.winehq.org/wine-builds/debian/ \
+    && apt-get update \
+    && DEBIAN_FRONTEND=noninteractive apt-get install --install-recommends winehq-stable -y \
+    && apt-get clean
+
+
+CMD ["/bin/bash"]
+WORKDIR /workdir

docker/linux-64/Dockerfile
@@ -45,34 +45,21 @@ bison \
 python-dev \
 libxtst-dev \
 xutils-dev \
-# for synfigstudio-nsis \
-unzip \
-# for portable versions \
-zip \
 # fix ca-certificates issue - https://github.com/multiarch/crossbuild/issues/63
 libssl1.0.2 openssl libgnutls30 \
 && apt-get clean
 
-COPY files/install-cross-tools.sh /
-RUN /install-cross-tools.sh
+#COPY files/install-cross-tools.sh /
+#RUN /install-cross-tools.sh
 #TODO: Remove above lines after Conan migration?
-
-RUN update-alternatives --set i686-w64-mingw32-gcc /usr/bin/i686-w64-mingw32-gcc-posix && \
-    update-alternatives --set i686-w64-mingw32-g++ /usr/bin/i686-w64-mingw32-g++-posix && \
-    update-alternatives --set x86_64-w64-mingw32-gcc /usr/bin/x86_64-w64-mingw32-gcc-posix && \
-    update-alternatives --set x86_64-w64-mingw32-g++ /usr/bin/x86_64-w64-mingw32-g++-posix
 
 RUN apt-get update && \
 DEBIAN_FRONTEND=noninteractive apt-get install -y -q \
 python3-pip ninja-build
 RUN pip3 install meson
 #RUN apt-get install -y -q  libpcre3-dev
-#RUN apt-get install -y -q wine64 wine32:i386
 RUN apt-get install -y -q doxygen
 RUN apt-get install -y -q graphviz
-RUN wget -qO - https://dl.winehq.org/wine-builds/winehq.key | apt-key add - && \
-apt-add-repository https://dl.winehq.org/wine-builds/debian/ && \
-apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install --install-recommends winehq-stable -y
 RUN apt-get install -y -q gfortran
 
 # Image metadata

docker/build.sh
@@ -6,3 +6,4 @@ DIRNAME=`pwd`
 
 ./linux-64/build.sh
 ./linux-32/build.sh
+./mingw/build.sh

docker/linux-64/build.sh
@@ -4,4 +4,4 @@ DIRNAME=`dirname "$0"`
 cd $DIRNAME
 DIRNAME=`pwd`
 
-docker build -t morevnaproject/builds-64 ./
+docker build -t morevnaproject/builds-linux64 ./

docker/mingw/build.sh
@@ -4,4 +4,4 @@ DIRNAME=`dirname "$0"`
 cd $DIRNAME
 DIRNAME=`pwd`
 
-docker build -t morevnaproject/builds-linux32 ./
+docker build -t morevnaproject/builds-mingw ./

env-builder-data/build/script/packet/cairo-1.15.4.sh
@@ -8,6 +8,7 @@ PK_LICENSE_FILES="AUTHORS COPYING COPYING-LGPL-2.1 COPYING-MPL-1.1"
 
 source $INCLUDE_SCRIPT_DIR/inc-pkall-default.sh
 
-if [ "$PLATFORM" = "linux" ] || [ ! -z "$IS_NATIVE" ]; then
-    DEPS="$DEPS xcbfull-1.12"
-fi
+# not needed for debian >= stretch
+#if [ "$PLATFORM" = "linux" ] || ( [ ! -z "$IS_NATIVE" ] && [[ "$NATIVE_PLATFORM" != "mingw" ]] ); then
+#    DEPS="$DEPS xcbfull-1.12"
+#fi

env-builder-data/build/script/packet/glib-2.69.3.sh
@@ -1,7 +1,4 @@
 DEPS="zlib-1.2.12 ffi-3.2.1 pcre-8.37"
-if [ "$PLATFORM" != "linux" ]; then
-    DEPS_NATIVE="glib-2.69.3"
-fi
 
 PK_DIRNAME="glib-2.69.3"
 PK_ARCHIVE="$PK_DIRNAME.tar.xz"
@@ -16,11 +13,11 @@ pkbuild() {
     cd "$BUILD_PACKET_DIR/$PK_DIRNAME"
     #sed -i 's|https://ftp.pcre.org|http://ftp.exim.org|g' subprojects/libpcre.wrap
     if [[ $TC_HOST == "i686-w64-mingw32" ]]; then
-        export WINEPATH="$ENVDEPS_PACKET_DIR/bin/"
+        export WINEPATH="$ENVDEPS_PACKET_DIR/bin/;$WINEPATH_BASE"
         export CROSS_FILE="--cross-file /build/script/meson/linux-mingw-w64-32bit.txt"
         patch "gio/tests/meson.build" "$FILES_PACKET_DIR/gio-meson-build-win32.patch" || return 1
     elif [[ $TC_HOST == "x86_64-w64-mingw32" ]]; then
-        export WINEPATH="$ENVDEPS_PACKET_DIR/bin/"
+        export WINEPATH="$ENVDEPS_PACKET_DIR/bin/;$WINEPATH_BASE"
         export CROSS_FILE="--cross-file /build/script/meson/linux-mingw-w64-64bit.txt"
     else
         export CROSS_FILE=""
@@ -41,7 +38,7 @@ pkinstall() {
 #        cat <<EOT >> ${INSTALL_PACKET_DIR}/bin/glib-compile-resources
 ##!/bin/bash
 #
-#glib-compile-resources "$@"
+#wine ${INSTALL_PACKET_DIR}/bin/glib-compile-resources.exe "\$@"
 #EOT
 #        chmod +x ${INSTALL_PACKET_DIR}/bin/glib-compile-resources
         sed -i 's|glib_compile_resources=.*|glib_compile_resources=glib-compile-resources|g' ${INSTALL_PACKET_DIR}/lib/pkgconfig/gio-2.0.pc

commit ba2487fc284a88f59a0b1258c4f50ad2273327fd

Fix Synfig not starting on Windows 7
The issue is introduced in 34c2178c661fdde7046d22bbffc4c72d3c5755fb

docker/mingw/Dockerfile
@@ -153,5 +153,8 @@ gfortran-mingw-w64-i686 \
 RUN update-alternatives --set i686-w64-mingw32-gfortran /usr/bin/i686-w64-mingw32-gfortran-posix && \
     update-alternatives --set x86_64-w64-mingw32-gfortran /usr/bin/x86_64-w64-mingw32-gfortran-posix
 
+# Required by pango-1.40.3 - patch for win 7
+RUN apt-get install -y -q gtk-doc-tools
+
 CMD ["/bin/bash"]
 WORKDIR /workdir

env-builder-data/build/script/packet/pango-1.40.3.files/win7-compatibility.patch
@@ -1,0 +1,46 @@
+From 20d56cfc56000194d2bcf90f57b3319eaa472af5 Mon Sep 17 00:00:00 2001
+From: Christoph Reiter <reiter.christoph@gmail.com>
+Date: Sat, 7 Apr 2018 06:33:43 +0200
+Subject: [PATCH 1/2] build: move usp10 before gdi32
+
+Without this pango on mingw64 tries to lookup up the Script* functions
+in gdi32 and fails. It already fails at the build stage because the
+introspection dump crashes with a missing entry point error.
+
+Moving usp10 before gdi32 makes things work.
+
+This might be related to the warning in the uniscribe docs:
+"Important Starting with Windows 8: To maintain the ability to run on
+Windows 7, a module that uses Uniscribe must specify Usp10.lib
+before gdi32.lib in its library list."
+---
+ pango/Makefile.am | 2 +-
+ pangowin32.pc.in  | 2 +-
+ 2 files changed, 2 insertions(+), 2 deletions(-)
+
+diff --git a/pango/Makefile.am b/pango/Makefile.am
+index 0ce84357..b360533d 100644
+--- a/pango/Makefile.am
++++ b/pango/Makefile.am
+@@ -393,7 +393,7 @@ libpangowin32_1_0_la_LDFLAGS = $(LIBRARY_LIBTOOL_OPTIONS)
+ libpangowin32_1_0_la_LIBADD =			\
+ 	libpango-$(PANGO_API_VERSION).la	\
+ 	$(GLIB_LIBS)				\
+-	-lgdi32 -lusp10
++	-lusp10 -lgdi32
+ libpangowin32_1_0_la_DEPENDENCIES =		\
+ 	libpango-$(PANGO_API_VERSION).la
+ libpangowin32_1_0_la_SOURCES =	\
+diff --git a/pangowin32.pc.in b/pangowin32.pc.in
+index 4c9b879f..34ea6a3c 100644
+--- a/pangowin32.pc.in
++++ b/pangowin32.pc.in
+@@ -8,5 +8,5 @@ Description: Win32 GDI font support for Pango
+ Version: @VERSION@
+ Requires: pango
+ Libs: -L${libdir} -lpangowin32-@PANGO_API_VERSION@
+-Libs.private: -lgdi32 -lusp10
++Libs.private: -lusp10 -lgdi32
+ Cflags: -I${includedir}/pango-1.0
+-- 
+2.16.2

env-builder-data/build/script/packet/pango-1.40.3.sh
@@ -6,3 +6,12 @@ PK_ARCHIVE="$PK_DIRNAME.tar.xz"
 PK_URL="https://download.gnome.org/sources/pango/1.40/$PK_ARCHIVE"
 
 source $INCLUDE_SCRIPT_DIR/inc-pkall-default.sh
+
+pkhook_prebuild() {
+    if [ "$PLATFORM" = "win" ]; then
+        cp --remove-destination "$UNPACK_PACKET_DIR/$PK_DIRNAME/pango/Makefile.am" "pango/Makefile.am" || return 1
+        cp --remove-destination "$UNPACK_PACKET_DIR/$PK_DIRNAME/pangowin32.pc.in" "pangowin32.pc.in" || return 1
+        patch -p1 -i "$FILES_PACKET_DIR/win7-compatibility.patch" || return 1
+        autoreconf -fi
+    fi
+}