7fc677200747f55acda95af59142a1f1c6ddee41
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2024-07-03 18:30:24 +0700

    fix: Make libjpeg-turbo and opencv build on windows

--- env-builder-data/build/script/packet/libjpeg-turbo-3.0.3.sh
+++ env-builder-data/build/script/packet/libjpeg-turbo-3.0.3.sh
@@ -9,13 +9,25 @@ source $INCLUDE_SCRIPT_DIR/inc-pkall-default.sh
 
 pkbuild() {
     cd "$BUILD_PACKET_DIR/$PK_DIRNAME" || return 1
-    
+
+    if [ "$PLATFORM" = "win" ]; then
+        cat > toolchain.cmake << EOF
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR X86)
+set(CMAKE_C_COMPILER /usr/bin/${TC_HOST}-gcc)
+set(CMAKE_RC_COMPILER /usr/bin/${TC_HOST}-windres)
+EOF
+        LOCAL_OPTIONS="$LOCAL_OPTIONS -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake"
+    fi
+
     if ! check_packet_function $NAME build.configure; then
         mkdir build && cd build
-        cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PACKET_DIR .. || return 1
-        make -j8
+        cmake -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$INSTALL_PACKET_DIR $LOCAL_OPTIONS .. || return 1
         set_done $NAME build.configure
     fi
+    
+    make -j${THREADS} || return 1
+    
 }
 
 pkinstall() {
