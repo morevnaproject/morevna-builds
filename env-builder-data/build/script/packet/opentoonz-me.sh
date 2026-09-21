7a87eec1f566491accc5190d263c6bec0eb7b6fd
036006 <036006@gmail.com>
2026-09-21 12:28:07 +0700

    opentoonz: change build branch to master

--- env-builder-data/build/script/packet/opentoonz-me.sh
+++ env-builder-data/build/script/packet/opentoonz-me.sh
@@ -2,7 +2,7 @@ source "$PACKET_SCRIPT_DIR/opentoonz-master.sh"
 
 PK_DIRNAME="opentoonz"
 PK_URL="https://github.com/morevnaproject-org/$PK_DIRNAME.git"
-PK_GIT_CHECKOUT="origin/testing"
+PK_GIT_CHECKOUT="origin/master"
 
 # Bundle FFmpeg next to the OpenToonz binary (bin/ffmpeg, bin/ffprobe).
 # 64-bit only: the ffmpeg dependency chain (x264, theora, vorbis, lame,
